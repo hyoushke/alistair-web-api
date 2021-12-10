d579ed194a27fd446b609302e1e1f2e059a95274
Hyoushke <hyoushke@gmail.com>
2021-12-10 21:15:10 +0800

    initial codes

--- Alistair.Web.Api/WeatherForecast.cs
+++ Alistair.Web.Api/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Alistair.Web.Api
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace Alistair.Web.Api
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
